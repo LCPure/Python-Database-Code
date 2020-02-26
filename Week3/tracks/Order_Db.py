31fdaaa16b0d14c24d7473f399c39b14f1962382
unknown <lynn_worthy_uophx@yahoo.com>
2020-02-26 01:16:27 -0500

    Python Database first commit

---
+++ Week3/tracks/Order_Db.py
@@ -0,0 +1,16 @@
+import xml.etree.ElementTree as ET
+import sqlite3
+
+conn = sqlite3.connect('trackdb.sqlite')
+cur = conn.cursor()
+
+
+
+
+cur.execute('''SELECT Track.title, Artist.name, Album.title, Genre.name
+FROM Track JOIN Genre JOIN Album JOIN Artist
+ON Track.genre_id = Genre.ID and Track.album_id = Album.id
+    AND Album.artist_id = Artist.id
+ORDER BY Artist.name LIMIT 3''')
+
+ conn.commit()
